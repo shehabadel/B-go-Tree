770224f3c8273eda5450ea153a012c4ee7b245b7
Shehab Adel <shehabeldinadel@gmail.com>
2024-07-17 22:22:23 +0300

    ex1.8

--- ch1.5/main.go
+++ ch1.5/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -24,11 +25,32 @@ func main() {
 	// 	fmt.Printf("%s", b)
 	// }
 
-	ex17()
+	// ex17()
+	ex18()
 }
 
 func ex17() {
 	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
+
+func ex18() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		fmt.Println(url)
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
