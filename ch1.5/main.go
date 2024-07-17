eb6c9941ed24a93447595b6ed998a1f719dbf06a
Shehab Adel <shehabeldinadel@gmail.com>
2024-07-17 22:24:56 +0300

    ex19

--- ch1.5/main.go
+++ ch1.5/main.go
@@ -26,7 +26,8 @@ func main() {
 	// }
 
 	// ex17()
-	ex18()
+	// ex18()
+	ex19()
 }
 
 func ex17() {
@@ -62,5 +63,26 @@ func ex18() {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
+	}
+}
+
+func ex19() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		fmt.Println(url)
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("\n %d \n", resp.StatusCode)
 	}
 }
