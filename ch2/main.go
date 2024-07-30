d25fe852f5d49bc7de952c99cc04b3c62af7a46d
Shehab Adel <shehabeldinadel@gmail.com>
2024-07-30 12:01:24 +0300

    ch2.3.0

---
+++ ch2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string
+	fmt.Println(s) // Prints ""
+
+	var sa []string
+	fmt.Println(sa)      //prints []
+	fmt.Println(len(sa)) //prints 0
+}
