ae6df9d704f2f22ce9393dcfa5692369647fa0f2
Shehab Adel <shehabeldinadel@gmail.com>
2024-08-03 22:25:58 +0300

    tempconv import

--- ch2.3/main.go
+++ ch2.3/main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"b-go-tree/ch2.3/tempconv"
 	"fmt"
+	"os"
+	"strconv"
 )
 
+var a = b + c // a initialized third, to 3
+var b = f()   // b initialized second, to 2, by calling f
+var c = 1     // c initialized first, to 1
+
+func f() int { return c + 1 }
 func main() {
-	fmt.Printf("%g\n", tempconv.BoilingC-tempconv.FreezingC) // "100" °C
-	boilingF := tempconv.CToF(tempconv.BoilingC)
-	fmt.Printf("%g\n", boilingF-tempconv.CToF(tempconv.BoilingC)) // "180" °F
-
-	var c tempconv.Celsius
-	var f tempconv.Fahrenheit
-
-	// fmt.Println(c == f) //compiler error type mismatch
-	fmt.Println(c == tempconv.Celsius(f))
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			os.Exit(1)
+		}
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+		fmt.Println(f, c, tempconv.FToC(f), tempconv.CToF(c))
+	}
 }
