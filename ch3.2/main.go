aff34d0a2a439ab973e7967e05dc6202d9da43e5
Shehab Adel <shehabeldinadel@gmail.com>
2024-08-10 22:54:10 +0300

    created webserver to serve svg

--- ch3.2/main.go
+++ ch3.2/main.go
@@ -3,8 +3,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math"
-	"os"
+	"net/http"
 )
 
 const (
@@ -19,31 +20,9 @@ const (
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
 func main() {
-	var text string = fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
-		"width='%d' height='%d'>", width, height)
-	for i := 0; i < cells; i++ {
-		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
-			text = text + fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
-		}
-	}
-	text = text + "</svg>"
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8006", nil))
 
-	f, err := os.OpenFile("out.svg", os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		panic(err)
-	}
-
-	defer f.Close()
-
-	if _, err = f.WriteString(text); err != nil {
-		panic(err)
-	}
 }
 
 func corner(i, j int) (float64, float64) {
@@ -63,4 +42,27 @@ func corner(i, j int) (float64, float64) {
 func f(x, y float64) float64 {
 	r := math.Hypot(x, y) // distance from (0,0)
 	return math.Sin(r) / r
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+
+	var text string = fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+			text = text + fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	text = text + "</svg>"
+	w.Header().Set("Content-Type", "image/svg+xml")
+	_, err := w.Write([]byte(text))
+	if err != nil {
+		fmt.Errorf("%s", err)
+	}
 }
