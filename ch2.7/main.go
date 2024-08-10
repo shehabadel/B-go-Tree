0df9c80df4e6c73f4b0684f44d543c8791827274
Shehab Adel <shehabeldinadel@gmail.com>
2024-08-10 15:50:46 +0300

    ch2.7

--- ch2.7/main.go
+++ ch2.7/main.go
@@ -7,8 +7,27 @@ var i int = 3
 func f() {
 	fmt.Println(i)
 }
+
+func k() int {
+	return 1
+}
+
+func g(x int) int {
+	return 1
+}
 func main() {
 	var i float64 = 1.0
 	f()
 	fmt.Println(i)
+
+	if x := k(); x == 0 {
+		fmt.Println(x)
+	} else if y := g(x); x == y {
+		fmt.Println(x, y)
+	} else {
+		fmt.Println(x, y)
+	}
+	// fmt.Println(x, y) //compile error, no access to x and y
+
+	// x and y scopes are limited to their if statements only
 }
