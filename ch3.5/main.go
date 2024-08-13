ced7cb73082a7de0e3f8806fb2fccf2644daad64
Shehab Adel <shehabeldinadel@gmail.com>
2024-08-13 22:22:03 +0300

    bytes and strings

--- ch3.5/main.go
+++ ch3.5/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"strings"
 	"unicode/utf8"
 )
 
@@ -34,4 +36,40 @@ func main() {
 	  001001 contributes to the code point
 	*/
 
+	a := basename("/a/v/b/c/d/e.d.go")
+	fmt.Println(a)
+	v := comma("1923234223")
+	fmt.Println(v)
+	fmt.Println(intsToString([]int{1, 2, 3})) // "[1, 2, 3]"
+
+}
+
+func basename(s string) string {
+	slash := strings.LastIndex(s, "/")
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+}
+
+func comma(s string) string {
+	n := utf8.RuneCountInString(s)
+	if n <= 3 {
+		return s
+	}
+	return comma(s[:n-3]) + "," + s[n-3:]
+}
+
+func intsToString(values []int) string {
+	var buf bytes.Buffer
+	buf.WriteByte('[')
+	for i, v := range values {
+		if i > 0 {
+			buf.WriteByte(',')
+		}
+		fmt.Fprintf(&buf, "%d", v)
+	}
+	buf.WriteByte(']')
+	return buf.String()
 }
