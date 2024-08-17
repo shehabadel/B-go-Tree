beedaae1632df863ee558e3c8345a1f6a91bf414
Shehab Adel <shehabeldinadel@gmail.com>
2024-08-17 15:04:00 +0300

    exercise 4.2

--- ch4.1/main.go
+++ ch4.1/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
 	"fmt"
 	"log"
 )
@@ -14,12 +17,35 @@ func main() {
 	)
 	symbol := [...]string{USD: "$", "€", GBP: "£", RMB: "¥"}
 
+	f := flag.String("size", "256", "size of bit words")
+
+	flag.Parse()
 	for i, v := range symbol {
 		log.Default().Println(i, v)
 	}
 
-	r := [...]int{99: -1}
-	for _, v := range r {
-		fmt.Printf("%d \t", v)
+	fmt.Println([]uint8("x"), []byte("x"), []string{"x"})
+
+	var c1 []byte
+
+	fmt.Println(*f)
+	switch *f {
+	case "256":
+		sum := sha256.Sum256([]byte("x"))
+		c1 = sum[:]
+	case "512":
+		sum := sha512.Sum512([]byte("x"))
+		c1 = sum[:]
+	case "384":
+		sum := sha512.Sum384([]byte("x"))
+		c1 = sum[:]
+	}
+	log.Default().Printf("%[1]x \t %[1]s", c1)
+
+}
+
+func setZero(ptr *[4]int) {
+	for i := range ptr {
+		ptr[i] = 0
 	}
 }
